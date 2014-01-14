2de644f9291b2506bd840111b7e4972b12c94085
元朔(CobbLiu) <jinxin.ljx@alibaba-inc.com>
2014-01-14 20:56:09 +0800

    modify list's code style

--- src/list.h
+++ src/list.h
@@ -12,43 +12,58 @@ struct list_head {
     struct list_head *prev;
 };
 
+
 #define LIST_HEAD_INIT(name) {&(name), &(name) }
 #define LIST_HEAD(name) \
     struct list_head name = LIST_HEAD_INIT(name)
 
-static inline void init_list_head(struct list_head *list)
+static inline void 
+init_list_head(struct list_head *list)
 {
     list->next = list;
     list->prev = list;
 }
 
-/* add new */
-static inline void __list_add(struct list_head *new,
-                              struct list_head *prev, struct list_head *next)
+/**
+ * add new element
+ */
+static inline void 
+__list_add(struct list_head *new,
+           struct list_head *prev, 
+           struct list_head *next)
 {
     next->prev = new;
     new->next  = next;
     new->prev  = prev;
     prev->next = new;
 }
-static inline void list_add(struct list_head *new, struct list_head *head)
+
+static inline void 
+list_add(struct list_head *new, struct list_head *head)
 {
     __list_add(new, head, head->next);
 }
-static inline void list_add_tail(struct list_head *new, struct list_head *head)
+
+static inline void 
+list_add_tail(struct list_head *new, struct list_head *head)
 {
     __list_add(new, head->prev, head);
 }
 
-/* delete element */
-static inline void list_del(struct list_head *entry)
+
+/**
+ * delete element 
+ */
+static inline void 
+list_del(struct list_head *entry)
 {
     struct list_head *prev = entry->prev;
     struct list_head *next = entry->next;
     next->prev = prev;
     prev->next = next;
 
-/** These are non-NULL pointers that will result in page faults  
+/**
+ *  These are non-NULL pointers that will result in page faults  
  *  under normal circumstances, used to verify that nobody uses  
  *  non-initialized list entries.  
  */ 
@@ -58,24 +73,31 @@ static inline void list_del(struct list_head *entry)
     entry->prev = LIST_POISON2;
 }
 
-/* judge */
-static inline int list_empty(struct list_head *head)
+/**
+ * judge 
+ */
+static inline int 
+list_empty(struct list_head *head)
 {
     return head->next == head;
 }
 
-/* list walker */
+/**
+ * list walker 
+ */
 #define prefetch(x) __builtin_prefetch(x)
 #define list_for_each(pos, head)  \
-    for(pos = (head)->next;  \
+    for(pos = (head)->next;       \
         prefetch(pos->next), pos != (head);  \
         pos = pos->next)
-#define list_for_each_safe(pos, n, head) \
+#define list_for_each_safe(pos, n, head)     \
     for (pos = (head)->next, n = pos->next;  \
-         pos != (head);  \
+         pos != (head);           \
          pos = n, n = pos->next)
 
-/* another list walker */
+/**
+ * another list walker 
+ */
 #define list_first_entry(ptr, type, member)  \
     list_entry((ptr)->next, type, member)
 #define list_next_entry(pos, member) \
@@ -85,8 +107,10 @@ static inline int list_empty(struct list_head *head)
          &pos->member != (head); \
          pos = list_next_entry(pos, member))
 
-/* get real struct */
+/**
+ * get real struct
+ */
 #define list_entry(ptr, type, member) \
     ((type *)((char *)(ptr) - (unsigned long)(&((type *)0)->member)))
 
-#endif  /* ALI_LIST_H */
+#endif
