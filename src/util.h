ea01f2db11b8ba7b6a5b3b5cfe3cca9257cc6920
CobbLiu <cobbliu@Alibaba.(none)>
2014-03-17 21:00:03 +0800

    add rbtree

---
+++ src/util.h
@@ -0,0 +1,21 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <stdint.h>
+
+typedef int8_t     int8_t;
+typedef uint8_t    uint8_t;
+typedef int16_t    int16_t;
+typedef uint16_t   uint16_t;
+typedef int32_t    int32_t;
+typedef uint32_t   uint32_t;
+typedef int32_t    int32_t;
+typedef uint32_t   uint32_t;
+typedef intptr_t   int_t;
+typedef uintptr_t  uint_t;
+
+#endif
